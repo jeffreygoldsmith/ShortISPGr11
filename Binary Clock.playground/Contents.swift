6a42606412f4178be76674e8a51293246d98db23
rsgc-goldsmith-j <jgoldsmith@rsgc.on.ca>
2016-04-04 14:33:04 -0400

    Fixed Algorithm and Print system
    Added decimal to binary algorithm and created a timer on a one second
    interval to control unixtime increment and time value prints.

--- Binary Clock.playground/Contents.swift
+++ Binary Clock.playground/Contents.swift
@@ -1,34 +1,34 @@
-import Cocoa
+import Foundation
+import XCPlayground
 
 /*
- * Name : Jeffrey Goldsmith
- * Date : Sunday, January 17, 2016
- * Teacher : Mr. Gordon
- * Class : ICS3U
- */
+* Name : Jeffrey Goldsmith
+* Date : 4 April 2016
+* Teacher : Mr. Gordon
+* Class : ICS3U
+*/
 
-import Cocoa
-
-let date = NSDate()     // Get local time data from computer
-let calendar = NSCalendar.currentCalendar()
-
-let components = calendar.components([ .Hour, .Minute, .Second], fromDate: date)    // Take system time and set variables to be equal to current component
-let hours = components.hour
-let minutes = components.minute
-let seconds = components.second
-
-let hourLength = 5
+let hourLength = 5 // Length of binary values (H/M/S)
 let minuteLength = 6
 let secondLength = 6
 
-var hourOuput = ""
+var hourOutput = "" // Empty strings to contain output values
 var minuteOutput = ""
 var secondOutput = ""
 
+var uTime = Int(NSDate().timeIntervalSince1970) - 3600 * 4 // Set unixtime value to be system unixtime with timezone offset
+
+
+//
+// Function to decode decimal time to binary and pad with zeros.
+//
 func decode(length: Int, type: Int, var output: String) -> String
 {
     var input = type
     
+    //
+    // Loop through decimal number until binary number is complete.
+    //
     for (var i = 0; i < length; i++)
     {
         let bit = input % 2
@@ -40,50 +40,51 @@ func decode(length: Int, type: Int, var output: String) -> String
     return output
 }
 
-decode(hourLength, type: hours, output: hourOuput)
-decode(minuteLength, type: minutes, output: minuteOutput)
-decode(secondLength, type: seconds, output: secondOutput)
 
+XCPSetExecutionShouldContinueIndefinitely()
 
-//// Create a new canvas
-//let canvas = Canvas(width: 400, height: 400)
 //
-////
-//// Draw background.
-////
+// BinaryClock -- Class to manage timer subroutine and time increment.
 //
-//canvas.fillColor = Color(hue: 215, saturation: 30, brightness: 60, alpha: 100)
-//canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: canvas.height)
-//
-////
-//// Display binary time.
-////
-//
-//canvas.textColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100) // Colour text white
-//
-//canvas.drawText(message: "Hours    :   " + binaryHours, size: 35, x: 75, y: 300)
-//canvas.drawText(message: "Minutes : " + binaryMinutes, size: 35, x: 75, y: 200)
-//canvas.drawText(message: "Seconds: " + binarySeconds, size: 35, x: 75, y: 100)
+class BinaryClock
+{
+    //
+    // BinaryClock -- startTimer() Function to initialize timer with one second interval.
+    //
+    func startTimer()
+    {
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "onTimer:", userInfo: nil, repeats: true) // Create new timer with one second interval
+    }
 
-//canvas
-//
-//let test = "testout"
-//var number = 16
-//var reverseOutput = ""
-//
-//for (var i = 0; i < 6; i++)
-//{
-//    var bit = number % 2
-//    number /= 2
-//    output += String(bit)
-//}
-//
-//for index in output.characters.indices
-//{
-//    var iChar = output[index]
-//    reverseOutput.insert(iChar, atIndex: reverseOutput.startIndex)
-//}
+    
+    //
+    // BinaryClock -- onTimer() Timer subroutine to increment and print updated binary time to console.
+    //
+    @objc func onTimer(timer:NSTimer!)
+    {
+        uTime++ // Increment time
+        
+        var uClone = uTime // Set new time value
+        
+        let seconds = uClone % 60 // Calculate seconds
+        uClone /= 60
+        
+        let minutes = uClone % 60 // Calculate minutes
+        uClone /= 60
+        
+        let hours = uClone % 24 // Calculate hours
 
+        
+        print(decode(hourLength, type: hours, output: hourOutput)) // Print hours
+        print(decode(minuteLength, type: minutes, output: minuteOutput)) // Print minutes
+        print(decode(secondLength, type: seconds, output: secondOutput)) // Print seconds
+        print("") // Print new line
+    }
+}
+
+var clock = BinaryClock() // Create new instance of BinaryClock class
+
+clock.startTimer() // Start timer subroutine
 
 
 
